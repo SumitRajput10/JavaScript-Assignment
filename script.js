80a970a7f21376a6c2554e7f81214b028e6c461d
Sumit Rajput <s@gmail.com>
2023-03-06 18:32:58 +0530

    Calculator.Added Arithmetic operations

---
+++ script.js
@@ -0,0 +1,32 @@
+let string = ""
+let buttons = document.querySelectorAll('button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click',(e) => {
+        if (e.target.innerHTML == "="){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == "C"){
+            string = "";
+            document.querySelector('input').value = ""; 
+        }
+        // else if (e.target.innerHTML == "Clear"){
+        //     string = function(){
+        //         for(i=0; i<string.length; i)
+        //     };
+        //     document.querySelector('input').value = ""; 
+        // }
+        else if (e.target.innerHTML == "π"){
+            π = eval(string);
+            document.querySelector('input').value = string; 
+        }
+        else{
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        
+    })
+});
+
+// let buttons = onclick = display(string)
