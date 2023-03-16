2a9ac144dd200e58b204c2cbfc5c143d8d397477
Sumit Rajput <s@gmail.com>
2023-03-16 18:08:33 +0530

    Added Button Changing functionality

--- script.js
+++ script.js
@@ -1,15 +1,139 @@
 let input = document.getElementById("input_data");
 let powerFlag = false;
-
-
-
+let DTRFlag = true;
+let deg_btn = document.querySelector(".deg_btn")
+let memData;
+let memoryFlag = false;
+let changeflag = true;
+let tempdata;
 
 //----------------Input Data----------------------------------------
 function addData(number) {
     input.value += number;
 }
+function DtR() {
+    if (DTRFlag) {
+        deg_btn.innerHTML = "RAD";
+        DTRFlag = false;
+    }
+    else {
+        deg_btn.innerHTML = "DEG";
+        DTRFlag = true;
+    }
+}
+//-----------------Memory Functions---------------------------------------
 
+
+// document.querySelectorAll('.memoryFunction').forEach((element)=>{
+//     element.addEventListener('click', ()=>{
+//         memoryFunction(element)
+//         console.log(true)
+//     })
+// })
+// function memoryFunction(ele) {
+//     let eleId = ele.attributes.id.value;
+//     if (eleId == "mp") {
+//         if (!memoryFlag) {
+//             memData = "";
+//             document.getElementById("mc").style.color = "Black";
+//             document.getElementById("mr").style.color = "Black";
+//             document.getElementById("ms").style.color = "Black";
+//             memoryFlag = true;
+//         }
+//         (memoryFlag) ? memData += eval(input.value) + "+" : NULL;
+//         input.value = "";
+//     }
+//     else if (eleId == "mm") {
+//         if (!memoryFlag) {
+//             memData = "";
+//             document.getElementById("mc").style.color = "Black";
+//             document.getElementById("mr").style.color = "Black";
+//             document.getElementById("ms").style.color = "Black";
+//             memoryFlag = true;
+//         }
+//         (memoryFlag) ? memData += eval(input.value) + "-" : NULL;
+//         input.value = "";
+//     }
+//     else if (eleId == "mr" && memoryFlag) {
+//         (input.value == "") ? memData = memData.substring(0, memData.length - 1) : memData += input.value;
+//         input.value = eval(memData);
+//     }
+//     else if (eleId == "mc" && memoryFlag) {
+//         memData = "";
+//         console.log("memory is cleared");
+//         document.getElementById("mc").style.color = "rgb(163, 163, 163)";
+//         document.getElementById("mr").style.color = "rgb(163, 163, 163)";
+//         document.getElementById("ms").style.color = "rgb(163, 163, 163)";
+//         memoryFlag = false;
+//     } else if (eleId == "ms" && memoryFlag) {
+//         memData = eval(memData);
+//         console.log(`Last calculation ${memData} stored successfully`);
+//     }
+// }
+
+
+//-----------------Trignometry---------------------------------------------------
+function calculateDeg(value) {
+    let deg = (Math.PI / 180) * value;
+    return deg;
+}
+document.getElementById('trigonometry').addEventListener('change', (evnt) => {
+    let value = evnt.target.value
+
+    if (DTRFlag) {
+        if (value == 'sin') {
+            input.value = Math.sin(calculateDeg(eval(input.value)))
+        }
+        else if (value == 'cos') {
+            input.value = Math.cos(calculateDeg(eval(input.value)))
+        }
+        else if (value == 'tan') {
+            input.value = Math.tan(calculateDeg(eval(input.value)))
+        }
+        document.getElementById('trigonometry').selectedIndex = 0;
+    }
+    if (DTRFlag == false) {
+        if (value == 'sin') {
+            input.value = Math.sin(input.value);
+        }
+        else if (value == 'cos') {
+            input.value = Math.cos(input.value);
+        }
+        else if (value == 'tan') {
+            input.value = Math.tan(input.value);
+        }
+        document.getElementById('trigonometry').selectedIndex = 0;
+    }
+})
+//----------------------------Functions----------------------------------------
+document.getElementById('function').addEventListener('change', (event) => {
+    let value = event.target.value
+    if (value == 'random') {
+        input.value = Math.random(input.value);
+    }
+    else if (value == 'floor') {
+        input.value = Math.floor(input.value);
+    }
+    else if (value == 'ceil') {
+        input.value = Math.ceil(input.value);
+    }
+    document.getElementById('function').selectedIndex = 0;
+})
 // ----------------------------First Row--------------------------
+const changeop = () => {
+    if (changeflag) {
+        document.getElementById("square").innerHTML = "X<sup>3</sup>";
+        document.getElementById("squareroot").innerHTML = "3√x";
+        document.getElementById("log2").innerHTML = "log<sub>10</sub>";
+        changeflag = false;
+    }
+    else {
+        document.getElementById("square").innerHTML = "X<sup>2</sup>";
+        document.getElementById("squareroot").innerHTML = "2√x";
+        document.getElementById("log2").innerHTML = "log<sub>2</sub>";
+        changeflag = true;
+    }
+}
 function pi() {
     input.value += Math.PI
 }
@@ -25,12 +149,16 @@ function del(flag) {
         let back = input.value
         input.value = back.substring(0, back.length - 1)
     }
-
+    input.placeholder = '0';
 }
 // ----------------------------Second Row--------------------------
-
 function sqr() {
-    input.value = Math.pow(eval(input.value), 2)
+    if (changeflag) {
+        input.value = Math.pow(eval(input.value), 2)
+    }
+    else {
+        input.value = Math.pow(eval(input.value), 3)
+    }
 }
 function fractional() {
     input.value = Math.pow(eval(input.value), -1)
@@ -44,10 +172,14 @@ function modulus() {
 function expo() {
     input.value = Math.exp(eval(input.value))
 }
-
 // ----------------------------Third Row--------------------------------------
 function sqrt() {
-    input.value = Math.sqrt(input.value)
+    if (changeflag) {
+        input.value = Math.sqrt(input.value)
+    }
+    else {
+        input.value = Math.cbrt(input.value)
+    }
 }
 function factorial() {
     let fact = 1;
@@ -56,7 +188,6 @@ function factorial() {
     }
     input.value = fact;
 }
-
 //-----------------------------Fourth row------------------------------------
 function power() {
     // input.value = Math.pow(input.value, eval(input.value))
@@ -70,7 +201,6 @@ function power() {
         input.placeholder = "Enter second number"
         powerFlag = true;
     }
-
 }
 //----------------------------Fifth row------------------------------------
 function ten() {
@@ -78,7 +208,12 @@ function ten() {
 }
 //----------------------------Sixth row------------------------------
 function log() {
-    input.value = Math.log2(input.value)
+    if (changeflag) {
+        input.value = Math.log2(input.value)
+    }
+    else {
+        input.value = Math.log10(input.value)
+    }
 }
 //-----------------------------Seventh row---------------------------------
 function ln() {
@@ -89,7 +224,6 @@ function plus_minus() {
     if (sign == '+' || sign == '-') input.value = input.value.substring(1)
     else input.value = '-' + input.value;
 }
-
 function equal() {
     if (powerFlag) {
         input.value = Math.pow(tempdata, eval(input.value))
